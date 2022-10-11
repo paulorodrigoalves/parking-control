9d9fdc74b53d165234e7ef69cefbf6eedc60aeb5
paulosoares <paulo.asoares@goias.gov.br>
2022-10-11 07:42:08 -0300

    Exemplo de JPQL na interface ParkingSpotRepository

--- src/main/java/com/api/parkingcontrol/repositories/ParkingSpotRepository.java
+++ src/main/java/com/api/parkingcontrol/repositories/ParkingSpotRepository.java
@@ -2,6 +2,8 @@ package com.api.parkingcontrol.repositories;
 
 import com.api.parkingcontrol.models.ParkingSpotModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
@@ -10,6 +12,10 @@ import java.util.UUID;
 public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID> {
 
     boolean existsByLicensePlateCar(String licensePlateCar);
+
+//    @Query("SELECT P.id is not null FROM ParkingSpotModel P WHERE P.parkingSpotNumber = :teste")
+//    boolean existsByParkingSpotNumber(@Param("teste") String parkingSpotNumber);
+
     boolean existsByParkingSpotNumber(String parkingSpotNumber);
     boolean existsByApartmentAndBlock(String apartment, String block);
 }
